f3780cb6afd23317adf0f82f21d7a117c318f922
BlackEdder <edder@tkwsping.nl>
2016-04-20 11:58:55 +0100

    Basic working gg.correlation

--- R/ggplot2_ext.R
+++ R/ggplot2_ext.R
@@ -1,5 +1,27 @@
 #' @export
-gg.correlation <- function( samples )
+gg.correlation <- function( samples, xcol, idcol, geom_diag = "histogram", geom_co = "point" )
 {
-  data
+  pars <- unique(samples[[idcol]])
+
+  for (par1 in pars)
+  {
+    for (par2 in pars)
+    {
+      # Need to call the standard stat, and pass it the geom
+      if (par1==par2)
+      {
+        gg <- ggplot2::ggplot(data=samples[(samples[[idcol]]==par1)]) +
+          ggplot2::geom_histogram(ggplot2::aes_string(x=xcol))
+        print(gg)
+      } else
+      {
+        gg <- ggplot2::ggplot(data=
+                   data.frame(list(par1=samples[(samples[[idcol]]==par1)],
+                        par2=samples[(samples[[idcol]]==par2)]))) +
+          ggplot2::geom_point(ggplot2::aes_string(x=par1,y=par2))
+        print(gg)
+      }
+    }
+  }
+  return(gg)
 }
